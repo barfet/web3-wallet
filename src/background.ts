89a16bd264ffd0170f1be5a1f8822375753567b7
Roman Borysenok <roman.borysenok@gmail.com>
2024-09-27 16:15:48 +0200

    build

---
+++ src/background.ts
@@ -0,0 +1,7 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Extension installed');
+  });
+  
+  // Add more background script logic here
+  console.log('Background script running');
+  export {};
